dee1c0e5c2dfd1f8b011522848c11ff01611032f
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-12 17:17:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const config = {
+export default {
   telegram: {
     bot_token: process.env.TELEGRAM_BOT_TOKEN,
     chat_id: process.env.TELEGRAM_CHAT_ID
+  },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY
   }
 }
-
-export default config
