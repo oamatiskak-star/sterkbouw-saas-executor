366e85b8878df46e8537bc677f4067fd57691128
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-12 22:07:13 +0100

    Update copier.js

--- remap/copier.js
+++ remap/copier.js
@@ -1,21 +1,26 @@
 import fs from "fs"
 import path from "path"
 
-function copyDir(src, dest) {
+function copyRecursive(src, dest) {
   if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true })
+
   for (const item of fs.readdirSync(src)) {
     const s = path.join(src, item)
     const d = path.join(dest, item)
-    if (fs.statSync(s).isDirectory()) copyDir(s, d)
-    else fs.copyFileSync(s, d)
+
+    if (fs.statSync(s).isDirectory()) {
+      copyRecursive(s, d)
+    } else {
+      fs.copyFileSync(s, d)
+    }
   }
 }
 
-export function executePlan(planFile, sourceRoot, workspaceRoot) {
+export function executeRemap(planFile, workspaceRoot) {
   const plan = JSON.parse(fs.readFileSync(planFile))
-  plan.forEach(item => {
-    const src = item.source
-    const dest = path.join(workspaceRoot, item.target, path.basename(src))
-    copyDir(src, dest)
-  })
+
+  for (const item of plan) {
+    const targetDir = path.join(workspaceRoot, item.target, path.basename(item.source))
+    copyRecursive(item.source, targetDir)
+  }
 }
