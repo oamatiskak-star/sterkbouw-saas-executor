27507add20c6d6f563a37bdaeb6af1cf9e49caff
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-16 10:09:03 +0100

    Update index.js

--- builder/index.js
+++ builder/index.js
@@ -1,18 +1,12 @@
-import { runRemap } from "../remap/remapEngine.js"
-import { buildDocuments } from "../agent/documents.js"
+import * as calculatiesBouw from "../executor/actions/calculaties_bouw.js"
 
 export async function runBuilder(task) {
-  console.log("BUILDER START", task.id)
+  console.log("BUILDER START", task.id || "geen-id")
 
-  if (task.action === "REMAP") {
-    await runRemap(task)
-    return
+  if (task.action === "calculaties:bouw") {
+    return await calculatiesBouw.run(task)
   }
 
-  if (task.action === "DOCUMENTS") {
-    await buildDocuments(task)
-    return
-  }
-
-  console.log("BUILDER GEEN ACTIE", task.action)
+  console.log("BUILDER GEEN ACTIE VOOR", task.action)
+  return { ok: false, reason: "ONBEKENDE_BUILDER_ACTIE" }
 }
