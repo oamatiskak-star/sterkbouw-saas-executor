48a2eb5f079a1971ba5785a44f0043edcccfc772
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-16 10:06:56 +0100

    Update actionRouter.js

--- executor/actionRouter.js
+++ executor/actionRouter.js
@@ -1,24 +1,15 @@
 import * as calculatiesBouw from "./actions/calculaties_bouw.js"
-import * as calculatiesEW from "./actions/calculaties_ew.js"
-import * as architectenBouwtekening from "./actions/architecten_bouwtekening.js"
-import * as planningGenereer from "./actions/planning_genereer.js"
-import * as documentenUpload from "./actions/documenten_upload.js"
-
 import { runBuilder } from "../builder/index.js"
 
 /*
 CENTRALE ACTION REGISTRY
-– Elke actie heeft exact één handler
-– Executor is leidend
-– Builder draait intern
+– Alleen acties die echt bestaan
+– Geen dode imports
+– Builder intern
 */
 
 const handlers = {
-  "calculaties:bouw": calculatiesBouw,
-  "calculaties:ew": calculatiesEW,
-  "architecten:bouwtekening": architectenBouwtekening,
-  "planning:genereer": planningGenereer,
-  "documenten:upload": documentenUpload
+  "calculaties:bouw": calculatiesBouw
 }
 
 /*
