686ef83548e2dc9d082633352c86e4b03ea5d2e5
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-16 19:35:33 +0100

    Update index.js

--- builder/index.js
+++ builder/index.js
@@ -1,10 +1,6 @@
 // builder/index.js
 import fs from "fs"
 import path from "path"
-import { fileURLToPath } from "url"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export async function runBuilder(payload) {
   const action = payload?.action || ""
