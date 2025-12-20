c1519ee8ece1a7f0cd6850cf48c54b8d14e3638d
oamatiskak-star <o.amatiskak@gmail.com>
2025-12-20 14:52:18 +0100

    Update telegramSender.js

--- integrations/telegramSender.js
+++ integrations/telegramSender.js
@@ -1,12 +1,20 @@
 import fetch from "node-fetch"
 
-export async function sendTelegram(chatId, text) {
+export async function sendTelegramMessage(chatId, text) {
+  if (!process.env.TELEGRAM_BOT_TOKEN) {
+    console.error("TELEGRAM_BOT_TOKEN ontbreekt")
+    return
+  }
+
   await fetch(
     `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ chat_id: chatId, text })
+      body: JSON.stringify({
+        chat_id: chatId,
+        text
+      })
     }
   )
 }
