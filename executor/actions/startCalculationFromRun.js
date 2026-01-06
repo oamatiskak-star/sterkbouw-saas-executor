d4af2bcfe811ab94a242c88f69c7edec9c2d9825
oamatiskak-star <o.amatiskak@gmail.com>
2026-01-06 10:11:48 +0100

    Add startCalculationFromRun function stub

---
+++ executor/actions/startCalculationFromRun.js
@@ -0,0 +1,8 @@
+export async function startCalculationFromRun({ task_id, project_id, payload }) {
+  console.log(
+    "[START_CALCULATION_FROM_RUN_STUB]",
+    task_id,
+    project_id,
+    payload
+  );
+}
